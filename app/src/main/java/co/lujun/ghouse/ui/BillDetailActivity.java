14f6a5f94ee4c1c1739ad10bd43d186342569d3a
whilu <lujun.byte@gmail.com>
2015-08-17 23:59:26 +0800

    add BillDetail view

---
+++ app/src/main/java/co/lujun/ghouse/ui/BillDetailActivity.java
@@ -0,0 +1,18 @@
+package co.lujun.ghouse.ui;
+
+import android.os.Bundle;
+
+import co.lujun.ghouse.R;
+import co.lujun.ghouse.ui.widget.SlidingActivity;
+
+/**
+ * Created by lujun on 2015/8/17.
+ */
+public class BillDetailActivity extends SlidingActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bill_detail);
+    }
+}
